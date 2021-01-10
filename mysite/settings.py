8247b804c002548a05890ede5e00d89653c3175c
Nicholas Ang <ang.tze.yong@gmail.com>
2021-01-10 15:31:29 +0800

    Removed secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '@m*a6lhst@25+1vw&^fvtg*l2gplh4diwt5jx#%y&+wcb@v#-e'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@m*a6lhst@25+1vw&^fvtg*l2gplh4diwt5jx#%y&+wcb@v#-e')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
